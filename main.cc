b665c418b87a1316a0161dfce26f12859c3896db
Tony Rode <rode_t@epita.fr>
2013-06-10 15:10:37 +0200

    first results

--- main.cc
+++ main.cc
@@ -1,157 +1,88 @@
-#include <iostream>
-#include <fstream>
+#include "tab_bounds.hh" // the structure containing the relative coordinates for each tab
+#include <vector>
+#include <map>
 
+#include <mln/core/image/image2d.hh>
+#include <mln/labeling/blobs.hh>
+#include <mln/labeling/colorize.hh>
+#include <mln/value/rgb8.hh>
+#include <mln/io/ppm/save.hh>
 
-//#include "../lib_olena/olena-2.0/milena/mln/geom/all.hh"
+#include <mln/io/pbm/load.hh>
+#include <mln/io/ppm/load.hh>
+#include <mln/make/neighb2d.hh>
+#include <mln/make/double_neighb2d.hh>
+#include <mln/literal/colors.hh>
 
-bool DEBUG_ = false;
-
-
-// Copyright (C) 2009 EPITA Research and Development Laboratory (LRDE)
-// This file is part of Olena.
-//
-// Olena is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free
-// Software Foundation, version 2 of the License.
-//
-// Olena is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
-//
-// As a special exception, you may use this file as part of a free
-// software project without restriction.  Specifically, if other files
-// instantiate templates or use macros or inline functions from this
-// file, or you compile this file and link it with other files to produce
-// an executable, this file does not by itself cause the resulting
-// executable to be covered by the GNU General Public License.  This
-// exception does not however invalidate any other reasons why the
-// executable file might be covered by the GNU General Public License.
-
-
-#include <mln/all.hh>
-//#include <mln/core/image/image2d.hh>
-//#include <mln/value/rgb8.hh>
-//#include <mln/data/fill.hh>
-//#include <mln/data/paste.hh>
-//#include <mln/core/routine/duplicate.hh>
-//#include <mln/literal/colors.hh>
-//#include <mln/opt/at.hh>
-//#include <mln/io/ppm/save.hh>
+template <typename N>
+void labelize(const mln::image2d<bool>& pic,
+              const N& nbh,
+              const std::string& filename)
+{
+  using namespace mln;
+  using value::rgb8;
+  unsigned n;
+  image2d<unsigned> lab = labeling::blobs(pic, nbh, n);
+  image2d<rgb8> out = labeling::colorize(rgb8(), lab, n);
+  io::ppm::save(out, filename);
+}
 
 
 
-//#include <tests/data.hh>
-//#include <doc/tools/sample_utils.hh>
-
-int main(int argc, char** argv)
+int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm as output
 {
   using namespace mln;
 
-
   if (argc < 3)
-    return 0;  // ou balancer sur "see help"
-
-  // \{
-  image2d<value::rgb8> ima(40, 40);
-  // \}
-
-  // \{
-  data::fill(ima, literal::red);
-  // \}
-
-  // \{
-  for (def::coord row = 20; row < 30; ++row)
-    for (def::coord col = 20; col < 30; ++col)
-      ima(point2d(row, col)) = literal::blue;
-  // \}
-
-  // \{
-  for (def::coord row = 20; row < 30; ++row)
-    for (def::coord col = 20; col < 30; ++col)
-      opt::at(ima, row, col) = literal::blue;  // rajouter un if pour dessiner le rectangle :)
-  // \}
-  io::ppm::save(ima, argv[2]);   // path to img
-
-  image2d<value::rgb8> ima2 = duplicate(ima);
-
-  // \{
-  image2d<value::rgb8> lena;
-  io::ppm::load(lena, argv[1]); // remplacer par "/small.ppm" <-> argv[1] ...
-  // \}
-
-  // \{
-  data::fill(ima, lena);
-  // \}
-  io::ppm::save(ima, argv[2]);
-
-  ima = ima2;
-  // \{
-  data::paste(ima, lena);
-  // \}
-  io::ppm::save(lena, argv[2]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-int main (int argc, char** argv)
-{
-  if (argc >= 3)
   {
-
-    std::ifstream src;
-    std::ofstream dst;
-
-    src.open(argv[1]);
-    dst.open(argv[2]);
-
-
-    if (!src.is_open() && !dst.is_open())
-    {
-      mln::image2d<value::rgb8> lena;
-      io::ppm::load(lena, MLN_IMG_DIR "../Training_GT/GT_0001.png");
-
-      box2d roi = make::box2d(20, 20, 40, 40);
-
-      data::fill((lena | roi).rw(), literal::green);
-
-
-    }
+    std::cout << "See the readme for the usage";
+    exit(0);
   }
 
+  image2d<bool> img;
+  mln::image2d<value::rgb8> out;
+  int color = 0;
+
+
+  // 1 object <-> 1 color (represented by an int)
+  /*
+     this map does the link between colored objects and their own tab in which
+     is stored the object
+   */
+  std::map<int, std::vector<std::vector<int> > > c_tab;
+
+
+  // link between colored objects and their relative position (coordonates)
+  std::map<int, std::vector<std::vector<s_tab_bounds> > > coordonate_tab;
+
+
+
+  // if ppm -> io::ppm::load, pbm -> io::pbm::load... etc
+
+  io::pbm::load(img, argv[1]);
+
+  labelize(img, c8(), argv[2]);
+
+  // End of phase 1 (the detection of all objects)
+
+
+
+  io::ppm::load(out, argv[2]);
+
+
+  // Détecter les x et y de chaque tab, initialiser les maps
+
+  for (def::coord row = geom::min_row(out); row < geom::max_row(out); ++row)
+    for (def::coord col = geom::min_row(out); col < geom::max_col(out); ++col)
+      {
+	// if nouvelle couleur ...
+
+      }
+
+
+
+
+
+  io::ppm::save(out, argv[2]);
+
 }
-*/
---
+++ tab_bounds.hh
@@ -0,0 +1,10 @@
+#ifndef TAB_BOUNDS_HH
+#define TAB_BOUNDS_HH
+
+
+struct s_tab_bounds {
+  int x;
+  int y;
+};
+
+#endif /* !TAB_BOUNDS_HH */
