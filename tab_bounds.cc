eeb3bb287fe6a0cb8cf5362139cefaaace5909e3
Tony Rode <rode_t@epita.fr>
2013-06-14 14:08:28 +0200

    alright

---
+++ tab_bounds.cc
@@ -0,0 +1,25 @@
+#include "tab_bounds.hh"
+
+
+int Tab_bounds::get_x()
+{
+  return x_;
+}
+
+
+int Tab_bounds::get_y()
+{
+  return y_;
+}
+
+
+
+void Tab_bounds::set_x(int x)
+{
+  x_ = x;
+}
+
+void Tab_bounds::set_y(int y)
+{
+  y_ = y;
+}
