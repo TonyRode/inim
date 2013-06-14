f0d373f1cf93638d6bb8c7d2892d6db7646cb6cb
Tony Rode <rode_t@epita.fr>
2013-06-14 14:08:07 +0200

    alright maaaan

--- main.cc
+++ main.cc
@@ -24,9 +24,6 @@
 // Typedefs
 
 
-// Forward declarations
-bool is_known (std::vector<Rgb> colors, Rgb px);
-
 
 
 
@@ -49,6 +46,21 @@ bool operator<(int first, Tab_bounds second)
 }
 
 
+// Forward declarations
+bool is_known (std::vector<Rgb> colors, Rgb px);
+bool is_black(Rgb px);
+
+
+void print_first_colored_obj(std::vector<Rgb> colors, std::map<Rgb, std::vector<std::vector<int> > > c_tab)
+{
+  for (int i = 0; i < c_tab[colors[0]].size(); ++i)
+  {
+    for (int j = 0; j < c_tab[colors[0]][0].size(); ++j)
+      if (c_tab[colors[0]][0][j] == 1)
+	std::cout << "*";
+    std::cout << std::endl;
+  }
+}
 
 
 template <typename N>
@@ -91,10 +103,13 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
 
 
   std::vector<Rgb> colors; // vector of known colors
-  Rgb* current_color; // value of the current pixel
+  Rgb* current_color; // pointer to value of the current pixel
 
 
   current_color = new Rgb(0, 0, 0);
+  //                      r  g  b
+
+
 
   // 1 object <-> 1 color (represented by an int)
   /*
@@ -113,7 +128,10 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
 
   io::pbm::load(img, argv[1]);
 
-  labelize(img, c8(), argv[2]);
+  labelize(img, c8(), argv[2]); // saves also the img
+
+
+  std::cout << "1" << std::endl;
 
   // End of phase 1 (the detection of all objects)
 
@@ -121,7 +139,14 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
 
   io::ppm::load(out, argv[2]);
 
+  std::cout << "2" << std::endl;
+
+
   // Détecter les x et y de chaque tab, initialiser les maps
+
+  std::cout << "Nouvelles couleurs détectées (on montre uniquement leur niveau de rouge)" << std::endl;
+
+  int nb_colors = 0; // = nb objects recognized
 
   for (def::coord row = geom::min_row(out); row < geom::max_row(out); ++row)
     for (def::coord col = geom::min_col(out); col < geom::max_col(out); ++col)
@@ -132,27 +157,32 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
 
       (*current_color).set_rgb(opt::at(out, row, col).red(), opt::at(out, row, col).green(), opt::at(out, row, col).blue());
 
-      if (!is_known(colors, *current_color))
+      if (!is_known(colors, *current_color) && !is_black(*current_color))
       {
-	Rgb* adding_it = new Rgb((*current_color).get_red(), (*current_color).get_green(), (*current_color).get_blue());
+
+	nb_colors++;
+	std::cout << (*current_color).get_red() << "; ";
+
+	Rgb* adding_rgb = new Rgb((*current_color).get_red(), (*current_color).get_green(), (*current_color).get_blue());
 	std::vector<std::vector<int> > new_tab;
 
 
 	new_tab.resize(80);
 
 	for (int i = 0; i < new_tab.size(); ++i)
-	  new_tab[i].resize(100);
+	  new_tab[i].resize(100);                      // + init to 0 values ?
 
-
-
-	colors.push_back(*adding_it); // color (corresponding to a new item) added to the list
-
-	Rgb b = *adding_it;
 
 
 	c_tab.insert(std::pair<Rgb, std::vector<std::vector<int> > >
-		     (b, new_tab));
+		     (*adding_rgb, new_tab));
 	// We create a tab which will contain the item to make it easier to manipulate (recognizing the shape of a quarter note)
+
+
+
+	colors.push_back(*adding_rgb); // color (corresponding to a new item) added to the list
+
+
 
 
 	Tab_bounds adding_it_bis;
@@ -160,16 +190,14 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
 	adding_it_bis.set_y(row);
 
 
-	// créer un rgb * const pour ajouter la pair.
-
-
-
 	coordonate_tab.insert(std::pair<Rgb, Tab_bounds>
-			      (b, adding_it_bis));
+			      (*adding_rgb, adding_it_bis));
 
       }
+
   }
 
+  std::cout << "3 (nb_colors = " << nb_colors << ")" << std::endl;
 
   // Here we have our y and initialised all stuffs
   // Now we must define our x for each tab
@@ -177,7 +205,7 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
   x = 0; // col
   y = 0; // line
 
-  for (def::coord col = geom::min_col(out); col < geom::max_col(out); ++col)
+  for (def::coord col = geom::max_col(out); col < geom::min_col(out); --col)
   {
     for (def::coord row = geom::min_row(out); row < geom::max_row(out); ++row)
     {
@@ -188,24 +216,13 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
       int a = coordonate_tab[*current_color].get_x();
 
       // pour virer le test si on est sur du noir && que le x est le plus petit pour cette couleur
-      if (is_known(colors, *current_color) && col < a)
-      {
-
-
-	// l'actualiser
-
-
-      }
-
-
-
-      //      ++y;
+      if (is_known(colors, *current_color) && col > a)
+	coordonate_tab[*current_color].set_x(col);
     }
-    //    ++x;
   }
 
 
-
+  std::cout << "4 (got x now)" << std::endl;
 
 
 
@@ -237,20 +254,61 @@ int main(int argc, char* argv[]) // works for pbm as input at the moment and ppm
   // Here x and y are well set for each tab
 
 
+  // ok here we go
 
+
+  // Initialization of each tab -> maybe
+
+
+  for (def::coord row = geom::min_row(out); row < geom::max_row(out); ++row)
+    for (def::coord col = geom::min_col(out); col < geom::max_col(out); ++col)
+    {
+      // set the current_color to current pixel color
+      (*current_color).set_rgb(opt::at(out, row, col).red(), opt::at(out, row, col).green(), opt::at(out, row, col).blue());
+
+
+      // pour virer le test si on est sur du noir && que le x est le plus petit pour cette couleur
+      if (is_known(colors, *current_color) && !is_black(*current_color))
+      {
+
+	//	std::map<Rgb, std::vector<std::vector<int> > > c_tab;
+
+	int a = coordonate_tab[*current_color].get_x();
+	int b = coordonate_tab[*current_color].get_y();
+
+	if (col - a > c_tab[*current_color].size())
+	  c_tab[*current_color].resize(c_tab[*current_color].size() + col - a);
+
+	if (row - b > c_tab[*current_color][0].size())
+	  for (int i = 0; i < c_tab[*current_color].size(); ++i)
+	    c_tab[*current_color][i].resize(c_tab[*current_color][i].size() + row - b);
+
+
+	(c_tab[*current_color])[col - a][row - b] = 1;
+	coordonate_tab[*current_color].set_x(col);
+      }
+
+      // FIXME SEGFAULT bizarre
+
+    }
+
+
+  std::cout << "5 Segfault ?" << std::endl;
 
 
 
   io::ppm::save(out, argv[2]);
+
+
+  print_first_colored_obj(colors, c_tab);
+
+  std::cout << "Segfault !" << std::endl;
 
 }
 
 
 bool is_known (std::vector<Rgb> colors, Rgb px)
 {
-  if (!px.get_red() && !px.get_green() && !px.get_blue())
-    return false;
-
   //  for (auto a = colors.begin (); a != colors.end (); ++a)
 
   for (std::vector<Rgb>::iterator a = colors.begin(); a != colors.end(); ++a)
@@ -265,3 +323,10 @@ bool is_known (std::vector<Rgb> colors, Rgb px)
 
 }
 
+
+bool is_black(Rgb px)
+{
+  if (px.get_red() == 0 && px.get_green() == 0 && px.get_blue() == 0)
+    return true;
+  return false;
+}
